e5c11aa2e1b47920073f4ac92269b49411ac3fb6
Anonineteen <118849122+Anonineteen@users.noreply.github.com>
2026-06-21 12:22:34 -0300

    atualiza intermediario.pl

--- intermediario.pl
+++ intermediario.pl
@@ -1,80 +1,121 @@
-% FATOS
-pai(carlos, joao).
-pai(ricardo, ana).
-pai(joao, maria).
-pai(joao, pedro).
+/* O nível 2 do trabalho tem como tema, as relações familiares de Star Wars.
+Nos fatos, apenas relações de pai e mãe(adotivo ou biológico) foram apresentados e 
+os gêmeos para caso de curiosidade. */
+% Fatos: 
 
-mae(luiza, joao).
-mae(helena, ana).
-mae(ana, maria).
-mae(ana, pedro).
+mae(princesa_leia_organa,jaina_solo).
+mae(princesa_leia_organa,jacen_solo).
+mae(princesa_leia_organa,anakin_solo).
+mae(mara_jade,ben_skywalker).
+mae(padme_amidala,luke_skywalker).
+mae(padme_amidala,princesa_leia_organa).
+mae(jobal_naberrie,padme_amidala).
+mae(shmi_skywalker,anakin_skywalker).
 
-irmao(pedro, maria).
-irma(ana, joao).
+mae_adotiva(breha_organa,princesa_leia_organa).
+mae_adotiva(beru_lars,luke_skywalker).
 
-% REGRAS
+pai(luke_skywalker,ben_skywalker).
+pai(han_solo,jaina_solo).
+pai(han_solo,jacen_solo).
+pai(han_solo,anakin_solo).
+pai(anakin_skywalker,luke_skywalker).
+pai(anakin_skywalker,princesa_leia_organa).
+pai(ruweee_naberrie,padme_amidala).
+pai(cliegg_lars,owen_lars).
 
-% X é filho(a) de Y
-filho_de(X, Y) :-
-    pai(Y, X);
-    mae(Y, X).
+pai_adotivo(cliegg_lars,anakin_skywalker).
+pai_adotivo(owen_lars,luke_skywalker).
+pai_adotivo(bail_organa,princesa_leia_organa).
 
-% X e Y são irmãos
-irmaos(X, Y) :-
-    pai(P, X),
-    pai(P, Y),
-    mae(M, X),
-    mae(M, Y),
-    X \= Y.
+gemeos(luke_skywalker,princesa_leia_organa).
+gemeos(jaina_solo,jacen_solo).
 
-% X e Y são os pais de Z
-pais_de(X, Y, Z) :-
-    pai(X, Z),
-    mae(Y, Z).
+%regras
 
-% X é neto(a) de Y
-neto_de(X, Y) :-
-    (pai(Z, X); mae(Z, X)),
-    (pai(Y, Z); mae(Y, Z)).
+filho(X,Y):-
+    (mae(Y,X);pai(Y,X));
+    (mae_adotiva(Y,X);pai_adotivo(Y,X)).
 
-% Avô ou avó de Y
-avo_geral(X, Y) :-
-    (pai(X, Z); mae(X, Z)),
-    (pai(Z, Y); mae(Z, Y)).
+/* Determina "filho" como adotivo ou biológico, não havendo diferencação,
+pois todas as regras seguintes de parentesco mais direto independem de genética. */
 
-% REGRAS RECURSIVAS
+avo(X,Y):-
+    filho(Y,Z),
+    filho(Z,X).
 
-% Caso base: pai ou mãe direto
-antepassado(X, Y) :-
-    pai(X, Y).
+conjuge(X,Y):-
+    ((pai(X,Z),mae(Y,Z));
+    (mae(X,Z),pai(Y,Z)));
+    ((pai_adotivo(X,Z),mae_adotiva(Y,Z));
+    (pai_adotivo(Y,Z),mae_adotiva(X,Z))).
 
-antepassado(X, Y) :-
-    mae(X, Y).
+/* A regra "conjuge" foi estruturada de modo que impeça que um pai ou mãe adotivo
+seja vinculado à um biológico, apenas por convenção do universo. Ou seja, em Star Wars
+não existem casais compostos dessa forma. */
 
-% Caso recursivo: ancestral indireto
-antepassado(X, Y) :-
-    pai(X, Z),
-    antepassado(Z, Y).
+neto(X,Y):-
+    filho(Z,Y),
+    filho(X,Z).
 
-antepassado(X, Y) :-
-    mae(X, Z),
-    antepassado(Z, Y).
+irmao(X,Y):-
+    (X\=Y),
+    (filho(X,Z),filho(Y,Z)).
 
-% Tamanho de uma lista
-tamanho_lista([], 0).
+tio(X,Y):-
+    filho(Y,Z),
+    irmao(Z,X).
 
-tamanho_lista([_|T], N) :-
-    tamanho_lista(T, N1),
+cunhado(X,Y):-
+    (conjuge(X,Z),
+    irmao(Z,Y));
+    (conjuge(Y,Z),
+    irmao(Z,X)).
+
+primo(X,Y):-
+    filho(X,Z),
+    filho(Y,W),
+    irmao(Z,W),
+    (X\=Y).
+
+sobrinho(X,Y):-
+    filho(X,Z),
+    irmao(Z,Y).
+
+descendente(X,Y):-
+    filho(X,Y).
+descendente(X,Y):-
+    filho(X,Z),
+    descendente(Z,Y).
+
+/* Determina se um elemento X do universo é descendente de um elemento Y,
+desconsiderando se existe ou não vinculo genético. */
+
+descendente_natural(X,Y):-
+    (mae(Y,X);pai(Y,X)).
+descendente_natural(X,Y):-
+    (pai(Z,X);mae(Z,X)),
+    descendente_natural(Z,Y).
+
+/* Determina se um elemento X do universo é descendente_natural de um elemento Y,
+sendo váldo apenas para relações sanguíneas, e por isso usa os fatos: pai e mãe. */
+
+descendente_civil(X,Y):-
+    descendente(X,Y),
+    \+descendente_natural(X,Y).
+
+/* Determina, primeiro, se um elemento X do universo é descendente de um elemneto Y,
+após, elimina todos que têm relações apenas formadas por descendência de sangue. Dessa
+forma, engloba somente as linhagens (nos intevalos determinados pelas variáveis) que tem,
+pelo menos, uma relação adotiva. */
+
+geracoes(X,Y,1):-
+    filho(X,Y).
+geracoes(X,Y,N):-
+    filho(X,Z),
+    geracoes(Z,Y,N1),
     N is N1 + 1.
 
-% Verifica se um elemento pertence à lista
-membro(X, [X|_]).
-
-membro(X, [_|T]) :-
-    membro(X, T).
-
-% Concatena duas listas
-concatena([], L, L).
-
-concatena([H|T], L2, [H|R]) :-
-    concatena(T, L2, R).
+/*geracoes(descendente, ancestral, distância do descendente para o ancestral na árvore)*
+Pode exibir mais de uma ramificação da árvore genealógica, visto que considera
+tanto as relações biológicas, quanto as adotivas. */
